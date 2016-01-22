4b93be79698c8a10fe7b65b1aa2d6d2362fa2494
wissamsaikali <wissam.saikali@elementn.com>
2016-01-22 10:13:08 +0200

    sss

--- scripts/push.js
+++ scripts/push.js
@@ -3,6 +3,7 @@
 write=nobody
 execute=authenticated 
   **/ 
- 
- /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"></xml>*#*#*/
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="controls_if" id="8" inline="false" x="278" y="31"></block></xml>*#*#*/
+if (false) {
+}
 			
