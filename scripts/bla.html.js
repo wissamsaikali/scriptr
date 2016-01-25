9ebebcae6454011e660cd8eb9eaf7f02bfe93888
wissamsaikali <wissam.saikali@elementn.com>
2016-01-25 16:13:23 +0200

    a

--- scripts/bla.html.js
+++ scripts/bla.html.js
@@ -3,5 +3,5 @@
 write=nobody
 execute=authenticated 
   **/ 
- /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"<b>bold</b>"},"scriptrdata":[]}}*#*#*/
-var content= '<b>bold</b>'; var response = apsdb.httpRespond(); response.write(content);response.close();			
+ /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"<b>blou</b>"},"scriptrdata":[]}}*#*#*/
+var content= '<b>blou</b>'; var response = apsdb.httpRespond(); response.write(content);response.close();			
