70d7173dacb4f964be4f9e33eb84316dc0222bc6
wissamsaikali <wissam.saikali@elementn.com>
2016-01-25 13:49:52 +0200

    S

--- scripts/mathBlockly.js
+++ scripts/mathBlockly.js
@@ -3,5 +3,5 @@
 write=nobody
 execute=authenticated 
   **/ 
- /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_return" id="1" inline="false" x="91" y="56"><value name="return"><block type="math_arithmetic" id="2" inline="true"><field name="OP">MINUS</field><value name="A"><block type="math_number" id="3"><field name="NUM">5</field></block></value><value name="B"><block type="math_number" id="4"><field name="NUM">4</field></block></value></block></value></block></xml>*#*#*/
-return 5 - 4			
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_return" id="1" inline="false" x="91" y="56"><value name="return"><block type="math_arithmetic" id="2" inline="true"><field name="OP">MULTIPLY</field><value name="A"><block type="math_number" id="3"><field name="NUM">5</field></block></value><value name="B"><block type="math_number" id="4"><field name="NUM">4</field></block></value></block></value></block></xml>*#*#*/
+return 5 * 4			
