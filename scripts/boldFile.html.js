3bcbed694720fee26ef86426faf287b448ace124
wissamsaikali <wissam.saikali@elementn.com>
2016-01-25 16:46:14 +0200

    Update boldFile.html.js
    a

--- scripts/boldFile.html.js
+++ scripts/boldFile.html.js
@@ -4,4 +4,4 @@ write=nobody
 execute=authenticated 
   **/ 
  /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"<b>bold</b>"},"scriptrdata":[]}}*#*#*/
-var content= '<b>bold</b>'; var response = apsdb.httpRespond(); response.write(content);response.close();			
+var content= '<b>bold w</b>'; var response = apsdb.httpRespond(); response.write(content);response.close();			
