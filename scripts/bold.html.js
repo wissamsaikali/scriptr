e8a355c40dc2ff6bade2c996f3762576914527ba
wissamsaikali <wissam.saikali@elementn.com>
2016-01-25 16:09:30 +0200

    b

---
+++ scripts/bold.html.js
@@ -0,0 +1,7 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"<b>bold</b>"},"scriptrdata":[]}}*#*#*/
+var content= '<b>bold</b>'; var response = apsdb.httpRespond(); response.write(content);response.close();			
