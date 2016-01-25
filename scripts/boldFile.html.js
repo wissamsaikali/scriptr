056b5b22957585dbe7ef4dcd911175705a75c7ad
wissamsaikali <wissam.saikali@elementn.com>
2016-01-25 16:53:21 +0200

    Delete boldFile.html.js

--- scripts/boldFile.html.js
+++
@@ -1,8 +0,0 @@
-/** Script ACLs do not delete 
- read=nobody 
-write=nobody
-execute=authenticated 
-  **/ 
- 
- /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"<b>bold</b>"},"scriptrdata":[]}}*#*#*/
-var content= '<b>bold w</b>'; var response = apsdb.httpRespond(); response.write(content);response.close();			
