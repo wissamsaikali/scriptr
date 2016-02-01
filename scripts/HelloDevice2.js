32fd458690398ebd992af87fcdaa319c3cfcfde3
wissamsaikali <wissam.saikali@elementn.com>
2016-02-01 11:06:22 +0200

    test

--- scripts/HelloDevice2.js
+++ scripts/HelloDevice2.js
@@ -3,14 +3,15 @@
 write=nobody
 execute=anonymous 
   **/ 
- //First, we'll require the 'http' library to use later for calling a 3rd party webservice
+ return 1;
+
+//First, we'll require the 'http' library to use later for calling a 3rd party webservice
 var http = require("http");
-   
+
 //Let's say your script is expecting an http request parameter called 'myName',
 //this is how you can retrieve ita
 var name = request.parameters.myName
-var file = request.files.file
-var file2 = request.files.file2
+                                                                                                                                                                             
 //Suppose you want to make that value available to future running script instances
 //you can store it in a persistent variable
 if (name != null) storage.local.theName = name
@@ -18,7 +19,7 @@ if (name != null) storage.local.theName = name
 //The value of 'storage.local.theName' is automatically stored in a persistent database.
 //So from now on, it will be available to all instances of this script to read an modify,
 //therefore you will not have to pass it as an http request parameter again.
- 
+
 //Persistent variables that start with 'storage.global' work the same 
 //but are available to all running script instances in your account .
 
@@ -30,25 +31,23 @@ var ip = request.headers["x-forwarded-for"];
 
 //The request object below will be used to make an HTTP call that will do a country lookup based on the IP 
 var requestObject = {
-  "url": "https://scriptr.io/hello",
-  "method": "PUT", // the method is optional, it defaults to GET
-   "files": {test:file, 
-            body:file2}
+ "url": "https://scriptr.io/hello",
+ "params": {"name": storage.local.theName, "ip": ip},
+ "method": "POST" // the method is optional, it defaults to GET.
 }
 
 //... and now we issue the request
 var country = null;
 var response = http.request(requestObject);
-return response;
 if(response.status == "200"){
  var result = JSON.parse(response.body);
  country = result.country;
-} 
+}
 
 //Finally, let's construct the response that this script will return
 var scriptResponse = "Hello";
 if(storage.local.theName != null) scriptResponse += " " + storage.local.theName;
 if(country != null) scriptResponse += " from " + country;
 scriptResponse += "!";
-                                                                                                                                 
-return scriptResponse;			
+
+return scriptResponse;//test			
