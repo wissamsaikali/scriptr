6b52b4d28668d871232e9edc2f1057d67c6b9073
wissamsaikali <wissam.saikali@elementn.com>
2016-02-10 17:44:21 +0200

    Update HelloDevice2.js
    aaa

--- scripts/HelloDevice2.js
+++ scripts/HelloDevice2.js
@@ -3,7 +3,7 @@
 write=nobody
 execute=anonymous 
   **/ 
- return 14;
+ return 15;
 
 //First, we'll require the 'http' library to use later for calling a 3rd party webservice
 var http = require("http");
